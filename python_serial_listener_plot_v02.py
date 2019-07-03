5215953b681fa320e19b8473454f2596d5c0b6de
raphaFanti <raphael.fanti@gmail.com>
2019-07-03 17:21:19 +0200

    implemented experiment data acquisition

--- python_serial_listener_plot_v02.py
+++ python_serial_listener_plot_v02.py
@@ -1,14 +1,18 @@
 import serial # import Serial Library
-import numpy  # Import numpy
+import numpy as np # Import numpy
 import matplotlib.pyplot as plt #import matplotlib library
 from drawnow import *
 
-# Create arrays for both readings
+# Create arrays for both readings, to be used on the graph
 chan1 = []
 chan2 = []
 
+# Create numpy arrays for storing the data experiment data
+experimentData = np.array(["TimeStamp", "Channel1","channel2"])
+
 # Opens serial port
-arduinoData = serial.Serial('com12', 9600) #Creating our serial object named arduinoData
+serialPort = "com12" # to be redefined according to the actual port used
+arduinoData = serial.Serial(serialPort, 9600) #Creating our serial object named arduinoData
 
 # Tell matplotlib you want interactive mode to plot live data
 plt.ion() 
@@ -39,23 +43,34 @@ while True: # While loop that loops forever
 	
 	# read line from serial and decode it
 	arduinoString = arduinoData.readline()
-	print(arduinoString)
 	arduinoString = arduinoString.decode(encoding = 'utf-8')
-	
-	#Convert data elements to float
 	dataArray = arduinoString.split(',')
-	dt1 = float(dataArray[0])            
-	dt2 = float(dataArray[1].strip("\r\n"))
+	print(dataArray)
 	
+	if dataArray[0] == "button_input": # identifies if message is from experiment
+		timeStamp = int(dataArray[1])
+		dt1 = float(dataArray[2])            
+		dt2 = float(dataArray[3].strip("\r\n")) # for some reason, carriage return and end of line need to me removed
+		experimentData = np.append(experimentData,[timeStamp, dt1, dt2], axis=0)
+		print("Experiment data logged")
+		#print(experimentData[-1])
+		
+	else: # in case the message is not from experiment, data is retrieved for plotting
 	
-	chan1.append(dt1)                     #Build our chan1 array by appending temp readings
-	chan2.append(dt2)
-	drawnow(makeFig)                       #Call drawnow to update our live graph
-	
-	plt.pause(.1)                     #Pause Briefly. Important to keep drawnow from crashing
-	
-	# remove points on data array to limit axe
-	cnt = cnt+1
-	if(cnt > visibleDataPoints):
-		chan1.pop(0)
-		chan2.pop(0)
+		#Convert data elements to float
+		
+		dt1 = float(dataArray[0])            
+		dt2 = float(dataArray[1].strip("\r\n"))
+		
+		
+		chan1.append(dt1)                     #Build our chan1 array by appending temp readings
+		chan2.append(dt2)
+		#drawnow(makeFig)                       #Call drawnow to update our live graph
+		
+		plt.pause(.1)                     #Pause Briefly. Important to keep drawnow from crashing
+		
+		# remove points on data array to limit axe
+		cnt = cnt+1
+		if(cnt > visibleDataPoints):
+			chan1.pop(0)
+			chan2.pop(0)
