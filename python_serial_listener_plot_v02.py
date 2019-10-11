cff7e72104c2de1ced5c1609bfcdec8d9eb70381
raphaFanti <raphael.fanti@gmail.com>
2019-10-11 10:29:00 +0200

    Included 5 channels on python listener

--- python_serial_listener_plot_v02.py
+++ python_serial_listener_plot_v02.py
@@ -15,9 +15,12 @@ visibleDataPoints = 50
 if plotLiveGraph:
 	graphChan1 = []
 	graphChan2 = []
+	graphChan3 = []
+	graphChan4 = []
+	graphChan5 = []
 
 # experiment data and time variables
-experimentData =  [["time", "chan1","chan2"]]
+experimentData =  [["time", "chan1","chan2","chan3","chan4","chan5"]]
 experimentTime = datetime.now()
 
 # initialization of state variable for recording data
@@ -31,11 +34,16 @@ def updateGraph():
 	plt.figure(1)
 	plt.title('Live strain gage data')
 	plt.grid(True) #Turn the grid on
-	plt.subplot(2, 1, 1)
-	plt.plot(graphChan1, label='Channel 1A')
-	plt.legend(loc='upper left')
-	plt.subplot(2, 1, 2)
-	plt.plot(graphChan2, label='Channel 1B')
+	plt.subplot(3, 2, 1)
+	plt.plot(graphChan1, label='Channel 1')
+	plt.subplot(3, 2, 2)
+	plt.plot(graphChan2, label='Channel 2')
+	plt.subplot(3, 2, 3)
+	plt.plot(graphChan3, label='Channel 3')
+	plt.subplot(3, 2, 4)
+	plt.plot(graphChan4, label='Channel 4')
+	plt.subplot(3, 2, 5)
+	plt.plot(graphChan5, label='Channel 5')
 	#plt.legend(loc='upper left')
 
 # function to store experiment data
@@ -52,12 +60,20 @@ def printExperimentGraph():
 	npExperimentData = np.array(experimentData)
 	npExperimentData = np.delete(npExperimentData, (0), axis=0)
 	print(npExperimentData)
+
 	plt.figure()
 	plt.title("Experiment recording" + experimentTime.strftime("%y-%m-%d_%Hh%Mm%Ss"))
-	plt.subplot(2, 1, 1)
-	plt.plot(npExperimentData[:,1], label='Channel 1A')
-	plt.subplot(2, 1, 2)
-	plt.plot(npExperimentData[:,2], label='Channel 1B')
+	plt.subplot(3, 2, 1)
+	plt.plot(npExperimentData[:,1], label='Channel 1')
+	plt.subplot(3, 2, 2)
+	plt.plot(npExperimentData[:,2], label='Channel 2')
+	plt.subplot(3, 2, 3)
+	plt.plot(npExperimentData[:,3], label='Channel 3')
+	plt.subplot(3, 2, 4)
+	plt.plot(npExperimentData[:,4], label='Channel 4')
+	plt.subplot(3, 2, 5)
+	plt.plot(npExperimentData[:,5], label='Channel 5')
+
 	fileName = experimentTime.strftime("%y-%m-%d_%Hh%Mm%Ss") + "_BootSensorGraph"
 	currentDir = os.getcwd()
 	dest = os.path.join(currentDir, "Data", fileName)
@@ -104,7 +120,7 @@ while True:
 	if serialMessage[0] == "button_pressed":
 		recording = not recording
 		if recording:
-			experimentData = [["time", "chan1","chan2"]] # numpy array for experiment data
+			experimentData = [["time", "chan1","chan2","chan3","chan4","chan5"]] # numpy array for experiment data
 			experimentTime = datetime.now()
 			arduinoData.write(str.encode("b"))
 			print("Recording experiment")
@@ -119,27 +135,39 @@ while True:
 		try:
 			data1 = float(serialMessage[0])
 			data2 = float(serialMessage[1])
+			data3 = float(serialMessage[2])
+			data4 = float(serialMessage[3])
+			data5 = float(serialMessage[4])
 			dataTime = datetime.now()
 		except:
 			data1 = ""
 			data2 = ""
+			data3 = ""
+			data4 = ""
+			data5 = ""
 			print("Problem decoding serial message. Value not stored")
 
 		# stores data on respective graph arrays, limits size of array
 		if plotLiveGraph:
 			graphChan1.append(data1)
 			graphChan2.append(data2)
+			graphChan3.append(data3)
+			graphChan4.append(data4)
+			graphChan5.append(data5)
 			drawnow(updateGraph) # update live graph
 		else:
-			print([data1, data2])
+			print([data1, data2, data3, data4, data5])
 
 		if recording:
-			experimentData.append([dataTime, data1, data2])
+			experimentData.append([dataTime, data1, data2, data3, data4, data5])
 
 	# controls for the length of graph array
 	if plotLiveGraph and len(graphChan1) > visibleDataPoints:
 		graphChan1.pop(0)
 		graphChan2.pop(0)
+		graphChan3.pop(0)
+		graphChan4.pop(0)
+		graphChan5.pop(0)
 
 if plotLiveGraph:
 	plt.pause(.1) #Pause Briefly. Important to keep drawnow from crashing
